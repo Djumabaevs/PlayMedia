f576b8e359f1eda0b1df7c0c05aee0280a5b7b6b
Bakyt <djumabaevb@gmail.com>
2021-04-19 13:07:08 +0600

    add seekBar change mediaPlayer to mp object

--- app/src/main/java/com/bignerdranch/android/playmedia/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/playmedia/MainActivity.java
@@ -6,18 +6,22 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     private MediaPlayer mediaPlayer;
     private Button playButton;
+    private SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         playButton = findViewById(R.id.play_button);
+        seekBar = findViewById(R.id.seek_bar_id);
 
         mediaPlayer = new MediaPlayer();
         try {
@@ -27,9 +31,17 @@ public class MainActivity extends AppCompatActivity {
         }
         //mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.party);
 
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                int duration = mp.getDuration();
+                Toast.makeText(MainActivity.this, String.valueOf((duration/1000)/60), Toast.LENGTH_LONG).show();
+            }
+        });
         MediaPlayer.OnPreparedListener preparedListener = new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(final MediaPlayer mp) {
+                seekBar.setMax(mp.getDuration());
                 playButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -46,6 +58,25 @@ public class MainActivity extends AppCompatActivity {
         };
         mediaPlayer.setOnPreparedListener(preparedListener);
         mediaPlayer.prepareAsync();
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(fromUser) {
+                    mediaPlayer.seekTo(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
  /*   public void pauseMusic() {
